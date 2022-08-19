85440c95f865099beb2d5e4550b1512407152067
Jonathan Lafrenière <46546590+jolafreniere@users.noreply.github.com>
2022-08-19 00:40:31 -0400

    first commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import WebSocket from 'ws';
+const ws:any = new WebSocket('ws://localhost:3000');
+
+ws.on('open', function open() {
+  ws.send('Client has opened a connection');
+});
+
+ws.on('message', function message(data) {
+  console.log('received: %s', data);
+});
+
+async function doStuff(){
+    while(true){
+        await new Promise(resolve => setTimeout(resolve, 3000));
+        ws.send("CLIENT MESSAGE: "+new Date().toISOString());
+    }
+}
+
+doStuff();
