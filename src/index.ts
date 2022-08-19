a748dbfafdb26f5deed5cc117f9735c5db3f773a
Jonathan Lafrenière <46546590+jolafreniere@users.noreply.github.com>
2022-08-19 01:08:52 -0400

    - Add logging of message sent to match server-side log format - Change delay between messages for one that differs from server to highlight asynchronous nature

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,10 @@ ws.on('message', function message(data) {
 
 async function doStuff(){
     while(true){
-        await new Promise(resolve => setTimeout(resolve, 2000));
-        ws.send("CLIENT MESSAGE: "+new Date().toISOString());
+        await new Promise(resolve => setTimeout(resolve, 2345));
+        let msg = `CLIENT MESSAGE: ${new Date().toISOString()}`;
+        console.log("sending: %s", msg);
+        ws.send(msg);
     }
 }
 
